08b9a69daec4183d85b66bb9fdadaf7c1382918d
shahrukh khan <shahrukh.khan@accenture.com>
2017-05-29 18:51:36 +0530

    adds activated route

--- debate-app/src/app/router/users/users.component.ts
+++ debate-app/src/app/router/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private active: ActivatedRoute, 
+    private route:Router) { }
 
   ngOnInit() {
+  }
+
+  showActivatedRoute() {
+    //this.route.navigate(['users'], {relativeTo: this.active})
   }
 
 }
